620c1aa642cf8b1cd74d43592739cc930bef855e
caosiyuan <17765103160@163.com>
2018-12-08 02:03:32 +0800

    React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
 import Main from './components/MainComponent';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-         <Main />
-      </div>
+        <BrowserRouter>
+            <div>
+                <Main />
+            </div>
+        </BrowserRouter>    
     );
   }
 }
